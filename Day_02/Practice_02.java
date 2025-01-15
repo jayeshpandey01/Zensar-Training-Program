ac78e1262862eebca311f8cf1947ad85c119a56b
Jayesh Pandey <116277383+jayeshpandey01@users.noreply.github.com>
2025-01-15 20:15:17 +0530

    number of keywords

--- Day_02/Practice_02.java
+++ Day_02/Practice_02.java
@@ -1,5 +1,3 @@
-// package Day_02;
-
 /*
 Keywords in java:
 1. abstract: It is a non-access modifier applicable for classes and methods. It is used to achieve abstraction in Java. An abstract class can't be instantiated, and it needs subclasses to provide implementations for its abstract methods. An abstract method is a method without a body.
@@ -17,6 +15,45 @@ Keywords in java:
 13. double: It is a data type that can store decimal numbers.
 14. else: It is used to specify the code block to be executed if the condition in an if statement is false.
 15. enum: It is used to define a fixed set of constants.
+16. extends: It is used to inherit from a class.
+17. final: It is a non-access modifier applicable for classes, methods, and variables. It is used to restrict the inheritance, overriding, and modification of a class, method, or
+variable, respectively.
+18. finally: It is used to create a block of code that follows a try block. The finally block is executed after the try block and any catch block.
+19. float: It is a data type that can store decimal numbers.
+20. for: It is used to create a for loop.
+21. if: It is used to create an if statement.
+22. implements: It is used to implement an interface.
+23. import: It is used to import classes from other packages.
+24. instanceof: It is used to test if an object is an instance of a class.
+25. int: It is a data type that can store whole numbers.
+26. interface: It is used to declare an interface in Java.
+27. long: It is a data type that can store large whole numbers.
+28. native: It is a non-access modifier applicable for methods. It is used to specify that a method is implemented in native code using JNI (Java Native Interface).
+29. new: It is used to create new objects.
+30. null: It is used to indicate that a reference variable does not refer to any object.
+31. package: It is used to declare a package in Java.
+32. private: It is an access modifier applicable for classes, methods, and variables. It is used to restrict the access of a class, method, or variable.
+33. protected: It is an access modifier applicable for classes, methods, and variables. It is used to restrict the access of a class, method, or variable to its subclasses and classes in the same package.
+34. public: It is an access modifier applicable for classes, methods, and variables. It is used to make a class, method, or
+variable accessible from any other class.
+35. return: It is used to return a value from a method.
+36. short: It is a data type that can store whole numbers from -32,768 to 32,767.
+37. static: It is a non-access modifier applicable for classes, methods, and variables. It is used to make a class, method, or
+variable belong to the class rather than instances of the class.
+38. strictfp: It is a non-access modifier applicable for classes and methods. It restricts floating-point calculations to ensure portability.
+39. super: It is used to refer to the superclass of a class.
+40. switch: It is used to create a switch statement.
+41. synchronized: It is used to create a synchronized block of code.
+42. this: It is used to refer to the current object.
+43. throw: It is used to throw an exception.
+44. throws: It is used to declare exceptions that a method can throw.
+45. transient: It is a non-access modifier applicable for variables. It is used to indicate that a variable should not be serialized.
+46. try: It is used to create a try block of code that can throw exceptions.
+47. void: It is used to specify that a method does not return a value.
+48. volatile: It is a non-access modifier applicable for variables. It indicates that a variable may be changed unexpectedly by other parts of the program.
+49. while: It is used to create a while loop.
+50. strictfp: It is a non-access modifier applicable for classes and methods. It restricts floating-point calculations to ensure portability.
+
  */
 
 public class Practice_02 {
